fab8151d24852d5be4c7d68f3e8657a02bf7d8ab
Steven <steven_891@icloud.com>
2024-07-11 20:08:39 +0800

    updated gitignore to ignore .vscode
    itemModel can now load prefab from custom path

--- Assets/Scripts/MenuView/ItemModel.cs
+++ Assets/Scripts/MenuView/ItemModel.cs
@@ -4,7 +4,8 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
-public class ItemModel : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHandler, IEndDragHandler {
+public class ItemModel : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHandler, IEndDragHandler
+{
     Guid guid; // this can be replaced with actual data class/struct
     [SerializeField] int index;
     [SerializeField] Vector2 originalPosition;
@@ -13,50 +14,59 @@ public class ItemModel : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHa
 
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         guid = Guid.NewGuid();
         index = transform.GetSiblingIndex();
         rectTransform = GetComponent<RectTransform>();
 
         Debug.Assert(rectTransform != null);
     }
-    public void bindTo(ListModel listModel) {
+    public void bindTo(ListModel listModel)
+    {
         gameObject.transform.SetParent(listModel.gameObject.transform);
         this.listModel = listModel;
         listModel.postDeleteActions += onDeleteAction;
         listModel.preInsertActions += onInsertAction;
     }
-    public static ItemModel spawnNewItem() {
+    public static ItemModel spawnNewItem(string prefabFilePath = "Prefabs/UI/item")
+    {
         //load from Resources/Prefabs/item
-        GameObject item = Instantiate(Resources.Load("Prefabs/UI/item", typeof(GameObject))) as GameObject;
+        GameObject item = Instantiate(Resources.Load(prefabFilePath, typeof(GameObject))) as GameObject;
         ItemModel itemModel = item.GetComponent<ItemModel>();
         return itemModel;
     }
-    void onDeleteAction(int deletedIndex) {
+    void onDeleteAction(int deletedIndex)
+    {
         Debug.Log("post delete: " + index);
         if (index < deletedIndex)
             return;
-        if (index == deletedIndex) {
+        if (index == deletedIndex)
+        {
             suicide();
             return;
         }
         index -= 1;
     }
-    void onInsertAction(int instertedIndex) {
+    void onInsertAction(int instertedIndex)
+    {
         if (index < instertedIndex) return;
         index += 1;
     }
-    void suicide() {
+    void suicide()
+    {
         listModel.postDeleteActions -= onDeleteAction;
         listModel.preInsertActions -= onInsertAction;
         Destroy(gameObject);
     }
 
-    public void OnDrag(PointerEventData eventData) {
+    public void OnDrag(PointerEventData eventData)
+    {
         GetComponent<RectTransform>().anchoredPosition += eventData.delta;
     }
 
-    public void OnDrop(PointerEventData eventData) {
+    public void OnDrop(PointerEventData eventData)
+    {
         GameObject droppedGameObject = eventData.pointerDrag;
         ItemModel droppedItemModel = droppedGameObject.GetComponent<ItemModel>();
         // TODO more robust
@@ -66,14 +76,17 @@ public class ItemModel : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHa
 
         listModel.swap(index, droppedIndex);
     }
-    public void setSiblingIndex(int index) {
+    public void setSiblingIndex(int index)
+    {
         transform.SetSiblingIndex(index);
         this.index = index;
     }
-    public int getIndex() {
+    public int getIndex()
+    {
         return index;
     }
-    public void OnBeginDrag(PointerEventData eventData) {
+    public void OnBeginDrag(PointerEventData eventData)
+    {
         Debug.Log("begin drag");
         Debug.Log("sibling index: " + transform.GetSiblingIndex());
         index = transform.GetSiblingIndex();
@@ -86,7 +99,8 @@ public class ItemModel : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHa
         GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
+    public void OnEndDrag(PointerEventData eventData)
+    {
         // canvas.sortingOrder--;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
         rectTransform.anchoredPosition = originalPosition;
