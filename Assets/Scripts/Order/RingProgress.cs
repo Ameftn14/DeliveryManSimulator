84a2f3498ff9f5d51a2c757c814f0ca48f897060
Tianyi <374832398@qq.com>
2024-07-12 01:45:05 +0800

    finaltest

--- Assets/Scripts/Order/RingProgress.cs
+++ Assets/Scripts/Order/RingProgress.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using System;
 
 public class RingProgress : MonoBehaviour
 {
+    public TimeSpan ddl;
+    public VirtualClockUI virtualClockUI;
     public SpriteRenderer sp_render; // 环形进度条的 SpriteRenderer 组件
     public float lifetime = 5f; // 存在时间，单位秒
+    public TimeSpan acceptTime;
     public int isAccept = 0; // 是否接受订单
     private float timer = 5f; // 计时器
     public bool shouldDestroy = false;
 
     void Start()
     {
+        ddl = transform.parent.GetComponent<SingleOrder>().Deadline;
+        virtualClockUI = GameObject.Find("Time").GetComponent<VirtualClockUI>();
         sp_render = GetComponent<SpriteRenderer>();
         SingleOrder singleOrder = transform.parent.GetComponent<SingleOrder>();
         if (singleOrder != null)
@@ -44,7 +50,21 @@ public class RingProgress : MonoBehaviour
             }
         }
         else{
-            sp_render.material.SetFloat("_Fill", 1);
+            //计算（截止时间-当前时间）/（截止时间-接收时间）
+            TimeSpan currentTime = virtualClockUI.GetTime();
+            TimeSpan lefttime = ddl - currentTime;
+            TimeSpan totaltime = ddl - acceptTime;
+            float Timeratio = (float)lefttime.TotalSeconds / (float)totaltime.TotalSeconds;
+            Debug.Log("Timeratio: " + Timeratio);
+            Debug.Log("currentTime: " + currentTime);
+            Debug.Log("ddl: " + ddl);
+            // 设置填充比例
+            sp_render.material.SetFloat("_Fill", Timeratio);
+            // 如果时间到了，销毁自身 GameObject
+            if (currentTime >= ddl)
+            {
+                shouldDestroy = true;
+            }
         }
     }
 }
