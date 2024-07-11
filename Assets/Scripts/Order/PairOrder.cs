636a170f191a6ee0ea17ff8c972b0bdbaebbd669
Tianyi <374832398@qq.com>
2024-07-11 16:29:16 +0800

    order能过编译

--- Assets/Scripts/Order/PairOrder.cs
+++ Assets/Scripts/Order/PairOrder.cs
@@ -1,111 +1,123 @@
-// using UnityEngine;
-// using UnityEngine.UI;
-// using System;
-// using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
 
-// public class PairOrder : MonoBehaviour
-// {
-//     public int OrderID;
-//     public SingleOrder from;
-//     public SingleOrder to;
+public class PairOrder : MonoBehaviour
+{
+    public int OrderID;
+    public GameObject OrderFrom;//预制件
+    public GameObject OrderTo;//预制件
+    public SingleOrder from;//单个订单:起点
+    public SingleOrder to;//单个订单:终点
 
-//     private float price;
-//     private float distance;
+    private int price;
+    private float distance;
 
-//     private Timespan Deadline;
-//     private Timespan AcceptTime;
+    private TimeSpan Deadline;
+    //private Timespan AcceptTime;
+    //private Timespan PickUpTime;
+    private float LifeTime = 5f;
 
-//     private int Orderstate;
+    private int Orderstate;
 
-//     public enum State
-//     {
-//         NotAccept,//未接单
-//         Accept,//已接单
-//         PickUp,//已取货
-//         Delivered,//已送达
-//         Finished//已完成        
-//     }
+    public enum State
+    {
+        NotAccept,//未接单
+        Accept,//已接单
+        PickUp,//已取货
+        Delivered,//已送达
+        Finished//已完成        
+    }
 
-//     public State state;
+    public State state;
 
-//     public Start()
-//     {
-//         state = State.NotAccept;
-//         distance = Vector2.Distance(from.GetPosition(), to.GetPosition());
-//         // TODO: 初始化
-//     }
+    public void Start()
+    {
+        state = State.NotAccept;
+        distance = Vector2.Distance(from.GetPosition(), to.GetPosition());
+        //获取脚本
+        from = OrderFrom.GetComponent<SingleOrder>();
+        to = OrderTo.GetComponent<SingleOrder>();
+        //随机生成价格
+        price = UnityEngine.Random.Range(30, 100);
+        // TODO: 初始化
+    }
 
-//     public Update()
-//     {
+    public void Update()
+    {
 
-//     }
+    }
 
-//     //下面是接口
+    public void GeneratePrefab()
+    {
+        //TODO: 生成预制件
+    }
+    //下面是接口
 
-//     public int GetOrderID()
-//     {
-//         return OrderID;
-//     }
-//     public void SetOrderID(int id)
-//     {
-//         this.OrderID = id;
-//     }
+    public int GetOrderID()
+    {
+        return OrderID;
+    }
+    public void SetOrderID(int id)
+    {
+        this.OrderID = id;
+    }
 
-//     public float GetPrice()
-//     {
-//         return price;
-//     }
-//     public void SetPrice(float price)
-//     {
-//         this.price = price;
-//     }
+    public float GetPrice()
+    {
+        return price;
+    }
+    public void SetPrice(int price)
+    {
+        this.price = price;
+    }
 
-//     public float GetDistance()
-//     {
-//         return distance;
-//     }
-//     public void SetDistance(float distance)
-//     {
-//         this.distance = distance;
-//     }
+    public float GetDistance()
+    {
+        return distance;
+    }
+    public void SetDistance(float distance)
+    {
+        this.distance = distance;
+    }
 
-//     public Timespan GetDeadline()
-//     {
-//         return Deadline;
-//     }
-//     public void SetDeadline(Timespan deadline)
-//     {
-//         this.Deadline = deadline;
-//     }
+    public TimeSpan GetDeadline()
+    {
+        return Deadline;
+    }
+    public void SetDeadline(TimeSpan deadline)
+    {
+        this.Deadline = deadline;
+    }
 
-//     public Timespan GetAcceptTime()
-//     {
-//         return AcceptTime;
-//     }
-//     public void SetAcceptTime(Timespan acceptTime)
-//     {
-//         this.AcceptTime = acceptTime;
-//     }
+    // public TimeSpan GetAcceptTime()
+    // {
+    //     return AcceptTime;
+    // }
+    // public void SetAcceptTime(TimeSpan acceptTime)
+    // {
+    //     this.AcceptTime = acceptTime;
+    // }
 
-//     public State GetState()
-//     {
-//         return state;
-//     }
-//     //StateChange
-//     public void OrederAccept()
-//     {
-//         state = State.Accept;
-//     }
-//     public void OrderPickUp()
-//     {
-//         state = State.PickUp;
-//     }
-//     public void OrderDelivered()
-//     {
-//         state = State.Delivered;
-//     }
-//     public void OrderFinished()
-//     {
-//         state = State.Finished;
-//     }
-// }
+    public State GetState()
+    {
+        return state;
+    }
+    //StateChange
+    public void OrederAccept()
+    {
+        state = State.Accept;
+    }
+    public void OrderPickUp()
+    {
+        state = State.PickUp;
+    }
+    public void OrderDelivered()
+    {
+        state = State.Delivered;
+    }
+    public void OrderFinished()
+    {
+        state = State.Finished;
+    }
+}
