2fbaa36196f1f12862fc7f240dae14322af2f52b
Ameftn14 <3220106034@zju.edu.cn>
2024-07-11 22:45:57 +0800

    UI added

--- Assets/Scripts/StartScene/StartCanvasBehaviour.cs
+++ Assets/Scripts/StartScene/StartCanvasBehaviour.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartCanvasBehaviour : MonoBehaviour
 {
