fd899dea447e6aaba647780155e9dd3d1d0787f1
Ameftn14 <3220106034@zju.edu.cn>
2024-07-13 03:52:19 +0800

    Remake map

--- Assets/Scripts/Edge/WayPointBehaviour.cs
+++ Assets/Scripts/Edge/WayPointBehaviour.cs
@@ -2,32 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPointBehaviour : MonoBehaviour
-{
+public class WayPointBehaviour : MonoBehaviour {
     public int pid = -1;
     public int startVid = -1, endVid = -1;
     public bool isBusy = false;
+    public int isResturant = 0;
     public MapManagerBehaviour mapManager = null;
 
     // 到startVertex的距离与整条边长度的比例
-    public float ratio = 0;
+    public float ratio = 0.5f;
 
-    void Awake()
-    {
-        if (mapManager == null)
-        {
+    void Awake() {
+        if (mapManager == null) {
             mapManager = transform.parent.parent.GetComponent<MapManagerBehaviour>();
         }
         Debug.Assert(mapManager != null);
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
     }
 
-    public void SetVertices(GameObject start, GameObject end)
-    {
+    public void SetVertices(GameObject start, GameObject end) {
         startVid = start.GetComponent<VertexBehaviour>().vid;
         endVid = end.GetComponent<VertexBehaviour>().vid;
         Debug.Assert(ratio >= 0 && ratio <= 1);
@@ -36,18 +32,15 @@ public class WayPointBehaviour : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    public void BecomeBusy()
-    {
+    public void BecomeBusy() {
         isBusy = true;
     }
 
-    public void BecomeFree()
-    {
+    public void BecomeFree() {
         isBusy = false;
     }
 }
