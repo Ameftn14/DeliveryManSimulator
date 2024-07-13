5d3767fe6842ceaa4622db23181f96e30b1ad315
FrostStar-F <1049800394@qq.com>
2024-07-13 12:59:43 +0800

    instance

--- Assets/Scripts/Settlement/getInformation.cs
+++ Assets/Scripts/Settlement/getInformation.cs
@@ -13,10 +13,17 @@ public class getInformation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = Property.speed;
-        int money = Property.money;
         if(Input.GetKeyDown(KeyCode.I)){
-            Debug.Log("speed is" + speed + "and money is" + money);
+            Debug.Log("speed is" + Property.speed + "and money is" + Property.money);
+        }
+        if(Input.GetKeyDown(KeyCode.O)){
+            Property.money += 10;
+            Debug.Log("Moneyup! Now money is" + Property.money);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            // 加载指定的场景
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
         }
         
     }
