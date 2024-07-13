4d0dadb3b43056840bf74f6eec660c5f5610e085
Tianyi <374832398@qq.com>
2024-07-13 16:04:35 +0800

    这什么鬼

--- Assets/Scripts/Order/PairOrder.cs
+++ Assets/Scripts/Order/PairOrder.cs
@@ -34,7 +34,16 @@ public class PairOrder : MonoBehaviour {
     public void Start() {
         generalManager = GameObject.Find("GeneralManager").GetComponent<GeneralManagerBehaviour>();
         orderDB = GameObject.Find("OrderDB").GetComponent<OrderDB>();
-        mapManager = GameObject.Find("MapManager").GetComponent<MapManagerBehaviour>();
+        if (!GameObject.Find("MapManager").TryGetComponent<MapManagerBehaviour>(out mapManager)) {
+            Debug.LogError("MapManager is not assigned!");
+            return;
+        }
+        else {
+            Debug.Log("The Dictionary Count:" + mapManager.GetWayPoints().Count);       
+            mapManager.LogtoDebug(0);
+            bool test = mapManager.GetWayPoints()[0].GetComponent<WayPointBehaviour>().isBusy;
+            Debug.Log("test is" + test);
+        }
         virtualClock = GameObject.Find("Time").GetComponent<VirtualClockUI>();
 
         state = State.NotAccept;
