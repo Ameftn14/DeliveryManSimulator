7f28264906d20d45e9466eb1f4dbee8dd1c9503f
FrostStar-F <1049800394@qq.com>
2024-07-15 02:30:14 +0800

    时间控制场景切换

--- Assets/Scripts/GamingScene/GamingCanvasBehaviour.cs
+++ Assets/Scripts/GamingScene/GamingCanvasBehaviour.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+using TMPro;
 
-public class GamingCanvasBehaviour : MonoBehaviour
-{
+public class GamingCanvasBehaviour : MonoBehaviour {
+    public OrderDB orderDB;
+    public VirtualClockUI virtualClockUI;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        orderDB = GameObject.Find("OrderDB").GetComponent<OrderDB>();
+        virtualClockUI = GameObject.Find("Time").GetComponent<VirtualClockUI>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        TimeSpan timespan = virtualClockUI.GetTime();
         // 检测空格键是否被按下
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (orderDB.IsClear() && timespan.Hours >= 19) {
             //Destroy(instance);
             // 加载指定的场景
             UnityEngine.SceneManagement.SceneManager.LoadScene("Settlement");
