58efb424420d51cc3dd2a88fb9dadc855fc59213
Ameftn14 <3220106034@zju.edu.cn>
2024-07-15 05:54:50 +0800

    debug无限兔兔

--- Assets/Scripts/Deliveryman/DeliverymanManager.cs
+++ Assets/Scripts/Deliveryman/DeliverymanManager.cs
@@ -13,16 +13,16 @@ public class DeliverymanManager : MonoBehaviour {
 
     public static int finishedcount = 0;
 
-    public static float speedUp = 25.0f;
-    public static float timeSlow = 25.0f;
+    public static float speedUp = 15.0f;
+    public static float timeSlow = 10.0f;
 
     public static int speedAvailable = 2;
     public static int capacityAvailable = 2;
     public static int speedUpAvailable = 2;
     public static int timeSlowAvailable = 2;
 
-    public static float addSpeedUp = 25.0f;
-    public static float addTimeSlow = 25.0f;
+    public static float addSpeedUp = 15.0f;
+    public static float addTimeSlow = 10.0f;
 
 
     // Start is called before the first frame update
